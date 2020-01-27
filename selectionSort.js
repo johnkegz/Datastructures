87a60d8f4f36f39f6c34d8df2de6ddd57fab719d
johnkegz <johnkal24@gmail.com>
2020-01-27 16:44:00 +0300

    bubble sort

---
+++ selectionSort.js
@@ -0,0 +1,16 @@
+function selectionSort(arr){
+    for(let i=0; i<arr.length; i++){
+        let smallest = i;
+      for(let j=i+1; j<arr.length; j++){
+        if(arr[j] < arr[smallest]) smallest = j
+      }
+      if(smallest !== i){
+        const temp = arr[smallest]
+        arr[smallest] = arr[i]
+        arr[i] = temp;
+      }
+    }
+    return console.log(arr)
+  }
+  
+  selectionSort([2,3,1,4,20,5,7,1,19]) 
